a1643ba880ba871ed2de0440dd1e058d5b12b39b
Bryan Dady <bryan@subsplash.com>
2024-02-18 22:30:09 -0700

    fix(terraform): terraform fmt
    Signed-off-by: Bryan Dady <bryan@subsplash.com>

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
 import {
-    to  = module.bryandady-com.cloudflare_zone.default[0]
-    id  = "2bdbe38f19173f5293010e1c56c18cf5"
+  to = module.bryandady-com.cloudflare_zone.default[0]
+  id = "2bdbe38f19173f5293010e1c56c18cf5"
 }
--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  account_id      = "31150904c04618c89d5ab49033304089"
+  account_id       = "31150904c04618c89d5ab49033304089"
   dmarc_quarantine = "v=DMARC1;p=quarantine;rua=mailto:ec74c5c0bf@rua.easydmarc.us;ruf=mailto:ec74c5c0bf@ruf.easydmarc.us;fo=1;"
 }
