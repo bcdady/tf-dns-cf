c79c848fcb9c081c3f8f9e24ae089d764d753464
Bryan Dady <bryan@subsplash.com>
2024-02-18 22:47:08 -0700

    fix(terraform): import _dmarc record

--- import.tf
+++ import.tf
@@ -2,3 +2,8 @@ import {
   to = module.bryandady-com.cloudflare_zone.default[0]
   id = "2bdbe38f19173f5293010e1c56c18cf5"
 }
+
+import {
+  to = module.bryandady-com.cloudflare_record.default["_dmarc-TXT-v=DMARC1;p=quarantine;rua=mailto:ec74c5c0bf@rua.easydmarc.us;ruf=mailto:ec74c5c0bf@ruf.easydmarc.us;fo=1;"]
+  id = "2bdbe38f19173f5293010e1c56c18cf5/4660f0e092b679cc6f1f0ccc0875aea0"
+}
