a62e8917124eb09d7cb72aca3d000b1dcfc2b842
Bryan Dady <bryan@subsplash.com>
2024-12-16 01:45:36 -0700

    chore(deps): bump req version and providers
    Signed-off-by: Bryan Dady <bryan@subsplash.com>

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = "~> 1.0"
 
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 3.23"
+      version = "~> 4.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.8"
+      version = "~> 0.8"
     }
   }
 }
